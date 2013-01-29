87d5010283d944bd6c618ed35eda25f7b001db50
Bryan W. Reid <bwreid@gmail.com>
2013-01-29 15:13:50 -0500

    Adding basic calc functions

---
+++ main.rb
@@ -0,0 +1,24 @@
+# ======================
+# Basic Calculator Functions
+# ======================
+
+def add(x,y)
+  x + y
+end
+
+def subtract(x,y)
+  x - y
+end
+
+def multiply(x,y)
+  x * y
+end
+
+def divide(x,y)
+  x.to_f / y.to_f
+end
+
+add(3,2)
+subtract(3,2)
+multiply(3,2)
+divide(3,2)

907e855e6b7001718fa04edc43f4de75192a33f1
Stephen Bae <donoage@gmail.com>
2013-01-29 15:48:32 -0500

    basic flow ver 1.01

--- main.rb
+++ main.rb
@@ -31,25 +31,41 @@ def squareroot (n1)
 end
 
 
-
+# ======================
 # calculator interface -- Matt Galvin
-print "Would you like to use a basic calculator or an advacnced calculator? Press 1 for basic, 2 for advanced. "
-  answer = gets.chomp
+# ======================
+
+print "Would you like to use a basic calculator or an advanced calculator? Press 1 for basic, 2 for advanced. "
+  answer = gets.chomp.to_i
+  # while answer != 1 || 2
+  #   puts "I don't understand. Please answer '1' or '2'. "
+  # answer = gets.chomp.to_i
+  # end
+
   if answer == 1
     print "Do you want to add, subtract, multiply or divide?"
-    answer = gets.chomp
-    print "What numbers would you like to use?"
-    numbers = gets.chomp.to_i
-      case basic
-        when add
-          #add function
-        when subtract
-          #subtract funciton
-        when multiply
-          #multiply function
-        when divide
-          #divide function
-        end
+    answer = gets.chomp.downcase
+    if answer == "add"
+
+      print "What's the first number you want to use? "
+      number1 = gets.chomp.to_i
+
+      print "What's the second number you want to use? "
+      number2 = gets.chomp.to_i
+
+      puts add(number1,number2)
+    end
+
+      # case answer
+      #   when add
+      #     add(number1, number2)
+      #   when subtract
+      #     subtract(number1, number2)
+      #   when multiply
+      #     multiply(number1, number2)
+      #   when divide
+      #     divide(number1, number2)
+      #   end
   else
     print "Do you want to raise to a power, or square root?"
     advanced = gets.chomp
@@ -61,5 +77,4 @@ print "Would you like to use a basic calculator or an advacnced calculator? Pres
         #square root function
       end
   end
-=======
 
